95cbb3e13a24fb179526103c7e1941349cf610ae
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-01-31 13:59:10 +0000

    'fixing the last ccheck on the 6th '

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,12 +5,13 @@
 
 class Rectangle:
     """Represent a rectangle.
+
     Attributes:
     number_of_instances (int): The number of instances in the rectangle.
     """
-    
+
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
