225ec170cc830b2e7eae760c2da153daad225382
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-02-07 12:02:01 +0000

    'obj from json '

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """Create a Python object from a JSON file."""
-    with open(filename "r" encoding="utf-8") as f:
-        return json.load(f)
+    with open(filename, "r", encoding="utf-8") as f:
+    return json.load(f)
