5d99b9a30479585b2037ee331d06628c80470dd0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-02-07 00:23:20 +0000

    'file write'

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,6 @@
 def write_file(filename="", text=""):
     """Return the number of lines in a text file."""
 
-
     with open(filename, "w+", encoding="utf-8") as f:
         f.write(text)
         charlen = len(text)
