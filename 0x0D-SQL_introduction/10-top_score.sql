94c52a886308e85d99ddb17bfc554f59d579c7e2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-03-13 16:29:50 +0000

    'edited the the 10th task '

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 --  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM hbtn_0c_0.second_table ORDER ASC
+SELECT * FROM `second_table` ORDER ASC
