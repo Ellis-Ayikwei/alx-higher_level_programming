c554992c23de2f8d9939e11fb5db42bc95689196
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-03-14 02:20:06 +0000

    'create user with privileges'

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,7 +5,7 @@
 CREATE USER 
     IF NOT EXISTS `user_0d_1`@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES
+GRANT ALL PRIVILeGES
     ON *.*
     TO `user_0d_1`@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
