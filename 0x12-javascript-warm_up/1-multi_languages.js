ae92b2747bca41d6dd36b827364c8c77b0a907e1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-04-08 16:23:38 +0000

    '3 languages'

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -5,4 +5,5 @@
 // The first line: “C is fun”
 // The second line: “Python is cool”
 // The third line: “JavaScript is amazing”
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
+
