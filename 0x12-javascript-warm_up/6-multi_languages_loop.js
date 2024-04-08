8f65b1f90381032fe8a90c78300563bc7cd496f4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-04-08 16:44:42 +0000

    'loop to lannguages '

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing  '];
+let i = 0;  // Loop counter
+
+while (i < languages.length) {
+  console.log(languages[i]);
+  i++;
+}
