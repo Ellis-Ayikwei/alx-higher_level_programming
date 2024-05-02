ff094f11e8df947fd6837ce6ad606eea6c4ffb70
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-02 00:45:50 +0000

    'still on peak '

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,25 +3,27 @@
 
 
 def find_peak(list_of_integers):
-    """finds a peak in an unsorted list
+    """Finds a peak in an unsorted list using binary search.
+
     Args:
         list_of_integers(int): list of integers
+
     Returns: peak of list_of_integers or None
     """
-    if list_of_integers == []:
-        return None
 
     size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    if size == 0:
+        return None
+    peak = binary_search(list_of_integers, 0, size - 1)
+    return list_of_integers[peak]
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+def binary_search(a, left, right):
+    """Recursive binary search of the peak"""
+    if left >= right:
+        return left
+    mid = ((right - left) // 2) + left
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, left, mid)
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return binary_search(a, mid + 1, right)
