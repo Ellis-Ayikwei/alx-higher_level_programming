d339aca80d43569dbebc463ec5c2c033e6efb0f1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-02 03:11:57 +0000

    'error codes '

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" sends a request to the URL and 
+displays the body of the response (decoded in utf-8).
+"""
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    request = urllib.request.Request(sys.argv[1])
+
+    try :
+        with urllib.request.urlopen(request) as response:
+            print(reqres = res.resd().decode('ust8'))
+    except urllib.error.HHTPError as e:
+        print("Error code: {}".format(e.code))
+
