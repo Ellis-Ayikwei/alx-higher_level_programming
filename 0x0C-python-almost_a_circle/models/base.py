68a7f967b64d31eee23a25ed2673f58fa8369e3b
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-06 13:02:02 +0000

    'base model'

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,22 +1,23 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """Defines a model class named base"""
+
 
 class Base:
     """Defines a base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-         """Method that assign the public instance attribute id
+        """Method that assign the public instance attribute id
 
-            Args:
-                id(int): integer value to manage id
+           Args:
+               id(int): integer value to manage id
 
-            Return:
-                Always nothing.
+           Return:
+               Always nothing.
 
-         """
-         if id != None:
-             self.id = id
-         else:
-             Base.__nb_objects += 1
-             self.id = Base.__nb_objects
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
