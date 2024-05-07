3dc480b71e717c088678369e21adfddf2730f5e2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-07 15:55:46 +0000

    'v2 tquare'

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,52 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y,
                         self.width))
+
+    # Getter and setter of width
+    @property
+    def width(self)
+    """getter for the width"""
+    return self.width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the size of width
+
+        Args:
+           value: Size to assign to the width
+
+        Return:
+           Always Nothing
+
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+
+    # Getter and setter of height
+    @property
+    def height(self):
+        """Getter the size of height
+        """
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """Setter the size of height
+
+        Args:
+           value: Size to assign to the height
+
+        Return:
+           Always Nothing
+
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.height = value
