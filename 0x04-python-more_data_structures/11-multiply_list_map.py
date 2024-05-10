91b8e6000be63f953e67a9ed29868720c482ac84
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-10 16:50:20 +0000

    edited the function

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 
-def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x*number, my_list)))
+def multiply_list_map(my_list=[], number=0):
+  return list(map(None, my_list, [number] * len(my_list)))
