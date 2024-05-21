baafb12f6f494a08c9695b9911cbdda56ddc1b16
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-21 14:25:52 +0000

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const StringToWrite = process.argv[3];
+
+function writeToFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+writeToFile(filepath, StringToWrite);
