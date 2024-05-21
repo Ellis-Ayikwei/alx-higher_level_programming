3480e66c7c82b4ad5a8b046b1220c38fbe298436
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-21 16:31:29 +0000

    chararcter appearance count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+//  script that prints the number of movies
+// where the character “Wedge Antilles” is present.
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const filteredFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+
+    const wedgeCount = filteredFilms.length;
+  }
+});
