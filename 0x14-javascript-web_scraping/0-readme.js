7449aeadafc14ad48290a850720817c993c87ab5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-21 14:06:41 +0000

    semistandar checked the file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-//script that reads and prints the content of a file
-const fs = require ('fs')
+// script that reads and prints the content of a file
+
+const fs = require('fs');
 
 const filePath = process.argv[2];
 
-function readFileAndPrint(filePath) {
+function readFileAndPrint (filePath) {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
