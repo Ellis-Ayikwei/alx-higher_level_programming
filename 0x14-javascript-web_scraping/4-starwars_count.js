435fb660d16a62f9cce1eeaedb9af76461f3319d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-22 11:59:26 +0000

    the count on wedge antiles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-//  script that prints the number of movies
-// where the character “Wedge Antilles” is present.
+// script that prints the number of movies where
+// the character “Wedge Antilles” is present.
 
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const filteredFilms = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
-
-    const wedgeCount = filteredFilms.length;
-    console.log(wedgeCount);
+request(url, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
